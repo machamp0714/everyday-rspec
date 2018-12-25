c6de0e271f8cda52c617f8d02ebc71ea5af0e3d9
25 macamp <rateliasudaisuki@gmail.com>
2018-12-25 22:41:38 +0900

    Add spec for valid user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it 'is valid with a first name, last name, email, and password' 
+    it 'is valid with a first name, last name, email, and password' do
+        user = User.new(
+            first_name: 'user',
+            last_name: 'test',
+            email: 'test@gmail.com',
+            password: 'password'
+        )
+        expect(user).to be_valid
+    end
 
     it 'is invalid without a first name'
 
