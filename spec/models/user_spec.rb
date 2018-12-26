52b5f0fbfbb36699bfdcd8fdc0611a44e5225f3d
25 macamp <rateliasudaisuki@gmail.com>
2018-12-26 23:23:19 +0900

    Refactoring user spec by FactoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,52 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
     it 'is valid with a first name, last name, email, and password' do
-        user = User.new(
-            first_name: 'user',
-            last_name: 'test',
-            email: 'test@gmail.com',
-            password: 'password'
-        )
+        user = FactoryBot.build(:user)
         expect(user).to be_valid
     end
 
     it 'is invalid without a first name' do
-        user = User.new(first_name: nil)
+        user = FactoryBot.build(:user, first_name: nil)
         user.valid?
         expect(user.errors[:first_name]).to include("can't be blank")
     end
 
     it 'is invalid without a last name' do
-        user = User.new(last_name: nil)
+        user = FactoryBot.build(:user, last_name: nil)
         user.valid?
         expect(user.errors[:last_name]).to include("can't be blank")
     end
 
     it 'is invalid with a duplicate email' do
-        User.create(
-            first_name: 'user',
-            last_name: 'test',
-            email: 'test@gmail.com',
-            password: 'password'
-        )
-        user = User.new(
-            first_name: 'user',
-            last_name: 'test',
-            email: 'test@gmail.com',
-            password: 'password'
-        )
+        FactoryBot.create(:user, email: 'factorybot-user@gmail.com')
+        user = FactoryBot.build(:user, email: 'factorybot-user@gmail.com')
         user.valid?
         expect(user.errors[:email]).to include("has already been taken")
     end
 
     it 'returns a user’s full name as a string' do
-        user = User.new(
-            first_name: 'test',
-            last_name: 'user',
-            email: 'test@gmail.com',
-            password: 'password'
-        )
+        user = FactoryBot.build(:user)
         expect(user.name).to eq "test user"
     end
 end
