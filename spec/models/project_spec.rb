f72d5704465d6a5f9ae8a0543a439ed974651f9d
25 macamp <rateliasudaisuki@gmail.com>
2018-12-26 22:51:09 +0900

    Refactoring project spec and Add validation test for name

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  # User単位では重複したプロジェク名を許可しないこと
-  it 'does not allow duplicate project names per user' do
-    user = User.create(
+  before do
+    @user = User.create(
       first_name: 'user',
       last_name: 'test',
       email: 'test@gmail.com',
       password: 'password'
     )
-    user.projects.create(
-      name: 'test-project'
+    @other_user = User.create(
+      first_name: 'other',
+      last_name: 'user',
+      email: 'other_user@gmail.com',
+      password: 'password'
     )
-    new_project = user.projects.build(
-      name: 'test-project'
+    @project = @user.projects.create(name: 'Test Project')
+  end
+  it 'is valid with a name' do
+    expect(@project).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    project = @user.projects.create(name: nil)
+    project.valid?
+    expect(project.errors[:name]).to include("can't be blank")
+  end
+  # User単位では重複したプロジェク名を許可しないこと
+  it 'does not allow duplicate project names per user' do
+    new_project = @user.projects.build(
+      name: 'Test Project'
     )
     new_project.valid?
     expect(new_project.errors[:name]).to include("has already been taken")
@@ -21,22 +36,7 @@ RSpec.describe Project, type: :model do
 
   # 二人のユーザーが同じ名前を使うことは許可する
   it 'allows two users to share a project name' do
-    user = User.create(
-      first_name: 'user',
-      last_name: 'test',
-      email: 'test@gmail.com',
-      password: 'password'
-    )
-    other_user = User.create(
-      first_name: 'other',
-      last_name: 'user',
-      email: 'other_user@gmail.com',
-      password: 'password'
-    )
-    user.projects.create(
-      name: 'Test Project'
-    )
-    other_project = other_user.projects.build(
+    other_project = @other_user.projects.build(
       name: 'Test Project'
     )
     expect(other_project).to be_valid
