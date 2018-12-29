32af15f20bd151e97420b6a1ce0e49127dbf9e99
25 macamp <rateliasudaisuki@gmail.com>
2018-12-29 15:52:58 +0900

    #1 Change projects_controller_spec
    createアクションのテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -70,4 +70,34 @@ RSpec.describe ProjectsController, type: :controller do
         end
     end
 
+    describe '#create' do
+        # 認証済みのユーザーとして
+        context 'as an authenticated user' do
+            before do
+                @user = FactoryBot.create(:user)
+            end
+            # プロジェクトを追加できること
+            it 'adds a project' do
+                project_params = FactoryBot.attributes_for(:project)
+                sign_in @user
+                expect {
+                    post :create, params: { project: project_params }
+                }.to change(@user.projects, :count).by(1)
+            end
+        end
+        # ゲストユーザーとして
+        context 'as a guest' do
+            it 'returns a 302 response' do
+                project_params = FactoryBot.attributes_for(:project)
+                post :create, params: { project: project_params }
+                expect(response).to have_http_status "302"
+            end
+            it 'redirects to the sign_in page' do
+                project_params = FactoryBot.attributes_for(:project)
+                post :create, params: { project: project_params }
+                expect(response).to redirect_to "/users/sign_in"
+            end
+        end
+    end
+
 end
