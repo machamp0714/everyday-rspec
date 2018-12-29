4c2a2e7251be83daf4617049875e49b431922378
25 macamp <rateliasudaisuki@gmail.com>
2018-12-29 14:44:14 +0900

    #1 Change projects_rspec
    add examples response index as an authenticated user

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+    before do
+        @user = FactoryBot.create(:user)
+    end
+    describe '#index' do
+        # 正常にレスポンスを返すこと
+        it 'responses successfully' do
+            sign_in @user
+            get :index
+            expect(response).to be_success
+        end
+
+        # 200レスポンスをかえすこと
+        it 'returns a 200 response' do
+            sign_in @user
+            get :index
+            expect(response).to have_http_status 200
+        end
+    end
 
 end
