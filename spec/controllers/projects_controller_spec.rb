0b45f3702cb528926f5a237cd92cbf1d498b0e19
25 macamp <rateliasudaisuki@gmail.com>
2018-12-29 21:56:59 +0900

    #1 Update projects_controller_spec
    projectが無効だった場合のテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -76,13 +76,23 @@ RSpec.describe ProjectsController, type: :controller do
             before do
                 @user = FactoryBot.create(:user)
             end
-            # プロジェクトを追加できること
-            it 'adds a project' do
-                project_params = FactoryBot.attributes_for(:project)
-                sign_in @user
-                expect {
-                    post :create, params: { project: project_params }
-                }.to change(@user.projects, :count).by(1)
+            context 'with valid attributes' do
+                it 'adds a project' do
+                    project_params = FactoryBot.attributes_for(:project)
+                    sign_in @user
+                    expect {
+                        post :create, params: { project: project_params }
+                    }.to change(@user.projects, :count).by(1)
+                end
+            end
+            context 'with invalid attributes' do
+                it 'dose not add a project' do
+                    project_params = FactoryBot.attributes_for(:project, :invalid)
+                    sign_in @user
+                    expect {
+                        post :create, params: { project: project_params }
+                    }.not_to change(@user.projects, :count)
+                end
             end
         end
         # ゲストユーザーとして
