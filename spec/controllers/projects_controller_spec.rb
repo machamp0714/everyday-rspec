1e694dc10aa2982cab88537ca24cffadee086cb2
25 macamp <rateliasudaisuki@gmail.com>
2018-12-29 14:47:05 +0900

    #1 Clean projects controller spec
    By use context

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
-    before do
-        @user = FactoryBot.create(:user)
-    end
     describe '#index' do
-        # 正常にレスポンスを返すこと
-        it 'responses successfully' do
-            sign_in @user
-            get :index
-            expect(response).to be_success
-        end
+        # 認証済みユーザーとして
+        context 'as an authenticated user' do
+            before do
+                @user = FactoryBot.create(:user)
+            end
+            # 正常にレスポンスを返すこと
+            it 'responses successfully' do
+                sign_in @user
+                get :index
+                expect(response).to be_success
+            end
 
-        # 200レスポンスをかえすこと
-        it 'returns a 200 response' do
-            sign_in @user
-            get :index
-            expect(response).to have_http_status 200
+            # 200レスポンスをかえすこと
+            it 'returns a 200 response' do
+                sign_in @user
+                get :index
+                expect(response).to have_http_status 200
+            end
         end
     end
 
