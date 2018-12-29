d5b87ade5e8c6f593f666f89082ce43e1a42c267
25 macamp <rateliasudaisuki@gmail.com>
2018-12-29 22:34:04 +0900

    Add tasks_controller_spec
    createアクションのテストを追加

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe TasksController, type: :controller do
         end
     end
 
+    describe '#create' do
+        it 'adds a new task to the project' do
+            new_task = { name: "New Task" }
+            sign_in @user
+            expect {
+                post :create, format: :json,
+                    params: { project_id: @project.id, task: new_task }
+            }.to change(@project.tasks, :count).by(1)
+        end
+    end
+
 end
