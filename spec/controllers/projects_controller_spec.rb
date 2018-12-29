d54c01a7af881dc1b9c8ded6d7d76a0ea346795c
25 macamp <rateliasudaisuki@gmail.com>
2018-12-29 16:57:52 +0900

    Change projects_controller_spec
    destroyアクションのテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -152,4 +152,55 @@ RSpec.describe ProjectsController, type: :controller do
             end
         end
     end
+
+    describe '#destroy' do
+        context 'as an authorized user' do
+            before do
+                @user = FactoryBot.create(:user)
+                @project = FactoryBot.create(:project, owner: @user)
+            end
+            it 'deletes a project' do
+                sign_in @user
+                expect {
+                    delete :destroy, params: { id: @project.id }
+                }.to change(@user.projects, :count).by(-1)
+            end
+        end
+        context 'as an unauthorized user' do
+            before do
+                @user = FactoryBot.create(:user)
+                other_user = FactoryBot.create(:user)
+                @project = FactoryBot.create(:project, owner: other_user)
+            end
+            it 'dose not delete a project' do
+                sign_in @user
+                expect {
+                    delete :destroy, params: { id: @project.id }
+                }.to_not change(Project, :count)
+            end
+            it 'redirects to the dashboard' do
+                sign_in @user
+                delete :destroy, params: { id: @project.id }
+                expect(response).to redirect_to root_path
+            end
+        end
+        context 'as a guest' do
+            before do
+                @project = FactoryBot.create(:project)
+            end
+            it 'responses a 302 reponse' do
+                delete :destroy, params: { id: @project.id }
+                expect(response).to have_http_status "302"
+            end
+            it 'redirects to the sign_in page' do
+                delete :destroy, params: { id: @project.id }
+                expect(response).to redirect_to "/users/sign_in"
+            end
+            it 'dose not delete a project' do
+                expect {
+                    delete :destroy, params: { id: @project.id }
+                }.not_to change(Project, :count)
+            end
+        end
+    end
 end
