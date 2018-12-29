ee84bd1fdb0ff49c752b881b4300da77f9497ac3
25 macamp <rateliasudaisuki@gmail.com>
2018-12-29 12:26:31 +0900

    Refactoring project spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,18 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   before do
-    @user = User.create(
-      first_name: 'user',
-      last_name: 'test',
-      email: 'test@gmail.com',
-      password: 'password'
-    )
-    @other_user = User.create(
-      first_name: 'other',
-      last_name: 'user',
-      email: 'other_user@gmail.com',
-      password: 'password'
-    )
+    @user = FactoryBot.create(:user)
+    @other_user = FactoryBot.create(:user)
     @project = @user.projects.create(name: 'Test Project')
   end
   it 'is valid with a name' do
