d1332290572874eb2c148e39cea9f4781b18ed70
25 macamp <rateliasudaisuki@gmail.com>
2018-12-31 20:37:35 +0900

    #7 Clean tasks_controller_spec
    letを使ってリファクタリング

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-    before do
-        @user = FactoryBot.create(:user)
-        @project = FactoryBot.create(:project, owner: @user)
-        @task = @project.tasks.create!(name: "Test Task")
-    end
+    let(:user) { FactoryBot.create(:user) }
+    let(:project) { FactoryBot.create(:project, owner: user) }
+    let(:task) { project.tasks.create!(name: "Test Task") }
 
     describe '#show' do
         it 'responds with JSON output' do
-            sign_in @user
+            sign_in user
             get :show, format: :json,
-                params: { project_id: @project.id, id: @task.id }
+                params: { project_id: project.id, id: task.id }
             expect(response.content_type).to eq "application/json"
         end
     end
@@ -19,24 +17,21 @@ RSpec.describe TasksController, type: :controller do
     describe '#create' do
         it 'adds a new task to the project' do
             new_task = { name: "New Task" }
-            sign_in @user
+            sign_in user
             expect {
                 post :create, format: :json,
-                    params: { project_id: @project.id, task: new_task }
-            }.to change(@project.tasks, :count).by(1)
+                    params: { project_id: project.id, task: new_task }
+            }.to change(project.tasks, :count).by(1)
         end
 
         it 'requires authentication' do
             new_task = { name: "New Task" }
             expect {
                 post :create, format: :json, params: {
-                    project_id: @project.id, task: new_task
+                    project_id: project.id, task: new_task
                 }
-            }.to_not change(@project.tasks, :count)
+            }.to_not change(project.tasks, :count)
             expect(response).to_not be_success
         end
     end
-
-    
-
 end
