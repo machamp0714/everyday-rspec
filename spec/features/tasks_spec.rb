1fa3fc477357770d8aa088fa17ce193835fb992f
25 macamp <rateliasudaisuki@gmail.com>
2018-12-31 22:32:48 +0900

    #7 Clean tasks_feature_spec
    ・セットアップをletを使って簡略化
    ・テストコード全体をメソッドに分割

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,27 +1,53 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  # ユーザーがタスクの状態を切り替える
-  scenario 'user toggles a task', js: true do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(
-      :project,
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project,
       name: "RSpec tutorial",
       owner: user
-    )
-    task = project.tasks.create!(name: "Finish RSpec tutorial")
+      )
+    }
+  let!(:task) { project.tasks.create!(name: "Finish RSpec tutorial") }
 
+  # ユーザーがタスクの状態を切り替える
+  scenario 'user toggles a task', js: true do
     sign_in user
+
+    go_to_project "RSpec tutorial"
+
+    complete_task "Finish RSpec tutorial"
+
+    expect_complete_task "Finish RSpec tutorial"
+    
+    undo_complete_task "Finish RSpec tutorial"
+
+    expect_incompleted_task "Finish RSpec tutorial"
+  end
+
+  def go_to_project(name)
     visit root_path
-    click_link "RSpec tutorial"
-    check "Finish RSpec tutorial"
+    click_link name
+  end
 
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to be_completed
+  def complete_task(name)
+    check name
+  end
 
-    uncheck "Finish RSpec tutorial"
+  def undo_complete_task(name)
+    uncheck name
+  end
 
-    expect(page).to_not have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to_not be_completed
+  def expect_complete_task(name)
+    aggregate_failures do
+      expect(page).to have_css "label.completed", text: name
+      expect(task.reload).to be_completed
+    end
+  end
+
+  def expect_incompleted_task(name)
+    aggregate_failures do
+      expect(page).to_not have_css "label.completed", text: name
+      expect(task.reload).to_not be_completed
+    end
   end
 end
